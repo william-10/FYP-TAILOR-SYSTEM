a95f2d0056e324af6e0b600236bae2d6321ca764
william-10 <willygeorge100@gmail.com>
2022-04-10 16:28:56 +0300

    created first modal

--- resources/views/dashboard/user/wishlist/view.blade.php
+++ resources/views/dashboard/user/wishlist/view.blade.php
@@ -4,6 +4,19 @@
 
 
 @section('content')
+
+<div id="mymodal" class="modal fade" role="dialog">
+    <div class="modal-dialog modal-sm">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button class="close" type="button" data-dismiss="modal">&times</button>
+                <h4 class="modal-title">Modal Header</h4>
+            </div>
+            <div class="modal-body">Body</div>
+        </div>
+    </div>
+</div>
+
 <div class="py-3 mb-4 shadow-sm bg-warning border-top ">
     <div class="container">
         <h5 class="mb-0">Collections/{{$products->categories->category_name}}/{{$products->name}}</h5>
@@ -14,7 +27,7 @@
         <div class="card-body">
             <div class="row">
                 <div class="col-md-4 border-right">
-                    <img src="{{asset('assets/uploads/product/'.$products->image)}}" class="w-100"alt="product image">
+                    <img src="{{asset('assets/uploads/product/'.$products->image)}}" class="w-100" alt="product image">
                 </div>
                 <div class="col-md-8">
                     <h2 class="mb-0">
@@ -30,107 +43,105 @@
                         {{ $products->description }}
                     </p>
                     <hr>
-                        @if ($products->qty >0)
-                            <label class="badge bg-success">In stock</label>
-                        @else
-                        <label class="badge bg-danger">Out of stock</label>
-                        @endif
+                    @if ($products->qty >0)
+                    <label class="badge bg-success">In stock</label>
+                    @else
+                    <label class="badge bg-danger">Out of stock</label>
+                    @endif
                     <div class="row mt-2">
                         <div class="col-sd-2">
                             <input type="hidden" value="{{ $products->id }}" class="prod_id">
                             <label for="Quantity">Quantity</label>
                             <div class="input-group text-center mb-3">
                                 <button class="input-group-text decrement-btn">-</button>
-                                <input type="text" name="quantity" value="1" style="width:30px" class="form control qty-input text-center" />
+                                <input type="text" name="quantity" value="1" style="width:30px"
+                                    class="form control qty-input text-center" />
                                 <button class="input-group-text increment-btn">+</button>
                             </div>
 
-                        <div class="row">
+                            <div class="row">
 
-                                                    <div class="col-md-2 mb-3">
-                                                        <label for="">bega</label>
-                                                        <input type="number"  class="form-control bega-input" autocomplete="off" value="0" name="bega">
-                                                    </div>
+                                <div class="col-md-2 mb-3">
+                                    <label for="">bega</label>
+                                    <input type="number" class="form-control bega-input" autocomplete="off" value="0"
+                                        name="bega">
+                                </div>
 
-                                                    <div class="col-md-2 mb-3">
-                                                        <label for="">mkono</label>
-                                                        <input type="number"  class="form-control mkono-input" autocomplete="off" value="0" name="mkono"></div>
+                                <div class="col-md-2 mb-3">
+                                    <label for="">mkono</label>
+                                    <input type="number" class="form-control mkono-input" autocomplete="off" value="0"
+                                        name="mkono">
+                                </div>
 
-                                                        <div class="col-md-2 mb-3">
-                                                        <label for="">kifua</label>
-                                                        <input type="number"  class="form-control kifua-input" autocomplete="off" value="0" name="kifua"></div>
+                                <div class="col-md-2 mb-3">
+                                    <label for="">kifua</label>
+                                    <input type="number" class="form-control kifua-input" autocomplete="off" value="0"
+                                        name="kifua">
+                                </div>
 
-                                                        <div class="col-md-2 mb-3">
-                                                        <label for="">urefu</label>
-                                                        <input type="number"  class="form-control urefujuu-input" autocomplete="off" value="0" name="urefu_juu"></div>
-
-
-
-
-                                                        <div class="col-md-2 mb-3">
-                                                        <label for="">kiuno</label>
-                                                        <input type="number"  class="form-control kiuno-input" autocomplete="off" value="0" name="kiuno"></div>
-
+                                <div class="col-md-2 mb-3">
+                                    <label for="">urefu</label>
+                                    <input type="number" class="form-control urefujuu-input" autocomplete="off"
+                                        value="0" name="urefu_juu">
+                                </div>
 
 
-                                                        <div class="col-md-2 mb-3">
-                                                        <label for="">paja</label>
-                                                        <input type="number"  class="form-control paja-input" autocomplete="off" value="0" name="paja"></div>
 
-                                                        <div class="col-md-2 mb-3">
-                                                        <label for="">urefu mguu</label>
-                                                        <input type="number"  class="form-control urefumguu-input" autocomplete="off"  value="0" name="urefu_mguu"></div>
+
+                                <div class="col-md-2 mb-3">
+                                    <label for="">kiuno</label>
+                                    <input type="number" class="form-control kiuno-input" autocomplete="off" value="0"
+                                        name="kiuno">
+                                </div>
+
+
+
+                                <div class="col-md-2 mb-3">
+                                    <label for="">paja</label>
+                                    <input type="number" class="form-control paja-input" autocomplete="off" value="0"
+                                        name="paja">
+                                </div>
+
+                                <div class="col-md-2 mb-3">
+                                    <label for="">urefu mguu</label>
+                                    <input type="number" class="form-control urefumguu-input" autocomplete="off"
+                                        value="0" name="urefu_mguu">
+                                </div>
+
+
+                            </div>
+
+                            <button type="button" class="btn btn-info" data-bs-toggle="modal"
+                                data-bs-target="#mymodal">measurement help</button>
+
 
 
                         </div>
+                    </div>
 
-                                  <button type="button" class="btn btn-info" data-toggle="modal" data-target="#mymodal" >measurement help</button>
-
-                                    <div id="mymodal" class="modal fade" role="dialog">
-                                        <div class="modal-dialog modal-sm">
-                                            <div class="modal-content">
-                                                <div class="modal-header">
-                                                    <button class="close" type="button" data-dismiss="modal">&times</button>
-                                                        <h4 class="modal-title">Modal Header</h4>
-                                                </div>
-                                                <div class="modal-body">Body</div>
-                                            </div>
-                                        </div>
-                                    </div>
-
-                        </div>
-                        </div>
-
-
-                        <div class="row mt-2">
+                    <div class="row mt-2">
                         <div class="col-md-10">
-                            hey
-                        </div>
-                        </div>
-
-                        <div class="row mt-2">
-                        <div class="col-md-10">
-                            <br/>
+                            <br />
 
                             @if ($products->qty >0)
-                                    <button type="button" class="btn btn-primary me-3       addTo-wish-cartBtn float-end">
-                                        <i class="fal fa-shopping-cart">Add to cart</i>
-                                     </button>
+                            <button type="button" class="btn btn-primary me-3       addTo-wish-cartBtn float-end">
+                                <i class="fal fa-shopping-cart">Add to cart</i>
+                            </button>
 
 
-                        @endif
+                            @endif
 
                             @if ($products->qty >0)
 
-                           @endif
-                            <button type="button" class="btn btn-success me-3 addToWishlist float-end">Add to wishlist</button>
+                            @endif
+                            <button type="button" class="btn btn-success me-3 addToWishlist float-end">Add to
+                                wishlist</button>
 
                         </div>
-                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
 </div>
 @endsection
-
