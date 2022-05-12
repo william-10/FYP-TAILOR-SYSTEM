9e6bb8da6aa11c852a9c249216d6f76cde76b31e
william-10 <willygeorge100@gmail.com>
2022-05-12 13:48:19 +0300

    map installing

--- resources/views/tailor/map/index.blade.php
+++ resources/views/tailor/map/index.blade.php
@@ -17,9 +17,9 @@ MAP
             @csrf
 <div class="row">
             <label for="address_address">Address</label>
-    <input type="text" id="address-input" name="address_address" class="form-control map-input">
-    <input type="hidden" name="address_latitude" id="address-latitude" value="0" />
-    <input type="hidden" name="address_longitude" id="address-longitude" value="0" />
+    <input type="text" id="address-input" name="address" class="form-control map-input">
+    <input type="hidden" name="latitude" id="address-latitude" value="0" />
+    <input type="hidden" name="longitude" id="address-longitude" value="0" />
 </div>
 <div id="address-map-container" style="width:100%;height:400px; ">
     <div style="width: 100%; height: 100%" id="address-map"></div>
